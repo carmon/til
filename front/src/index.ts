de5f553b57e7228289384e8a5aa185abbf9fe640
Carmon <elcarmon@gmail.com>
2020-12-27 19:08:56 -0300

    Update theme, add link to files

--- front/src/index.ts
+++ front/src/index.ts
@@ -1,22 +1,15 @@
 import fetch from 'node-fetch';
 
 const root = document.getElementById("root");
-
-const title = document.createElement("h1");
-title.textContent = "TIL - a personal blog";
-root.append(title);
-
-const parent = document.createElement("div");
-parent.className = "posts"
-root.append(parent);
+const posts = document.createElement("div");
+posts.className = "posts";
+root.append(posts);
 
 fetch('api/get-posts', { method: 'GET' }).then(async res => {
     const json = await res.json();
     const tags = json.reduce(
         (prev, curr) => 
-            `${prev}<zero-md src="posts/${curr}"></zero-md>`,
-        '');
-    const div = document.createElement("div");
-    div.innerHTML = tags;
-    parent.append(div);
+            `${prev}<zero-md src="posts/${curr}"></zero-md><a class="data" href="./posts/${curr}">${curr}</a>`,
+        '<h1>TIL - a personal blog</h1>');
+    posts.innerHTML = tags;
 });
